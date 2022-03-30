d38a840b443ab1c72bf53c288cc1959121b417d1
Alex Z <a.zarudnyy@matterlabs.dev>
2022-03-30 11:20:28 -0600

    Update and unify the long return flag location
    (cherry picked from commit f011c162c3d15add830b42357f5e29555ce9d8d9)

--- src/abi/memory.rs
+++ src/abi/memory.rs
@@ -7,3 +7,6 @@ pub const OFFSET_HEADER: usize = 0;
 
 /// The call and return data offset (cells).
 pub const OFFSET_DATA: usize = 1;
+
+/// The long-return flag offset (cells).
+pub const OFFSET_LONG_RETURN: usize = 0xffff;
